1bb05b0504a6703bf263b9f126634ba5cc3eb77b
serhiy-pope <sergey.pope@gmail.com>
2022-04-29 07:03:47 +0000

    added basic markup, style and js

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let today = new Date();
+let dd = String(today.getDate());
+let mm = String(today.getMonth() + 1); //January is 0!
+let yyyy = today.getFullYear();
+today = dd + '/' + mm + '/' + yyyy;
+document.getElementById("current_date").innerHTML = today;
+
+const input = document.querySelector("#add");
+const  btn = document.querySelector("#btn");
+const list = document.querySelector("#list");
+let el = document.getElementsByTagName('li');
+
+// this function will allow us to add elements when we click the button
+btn.onclick = function(){    
+    let txt = input.value;
+    if(txt ==''){
+      alert('you must write something');
+    }else{
+      li = document.createElement('li');
+      li.innerHTML = txt;
+      list.insertBefore(li,list.childNodes[0])
+      removeBtn = document.createElement('button');
+      removeBtn.innerHTML = "remove";
+      li.append(removeBtn)
+      input.value = '';
+    }
+    
+};
+
+//this function will allow us to check the clicked elements
+list.onclick = function(ev){
+    if(ev.target.tagName == 'LI'){
+         ev.target.classList.toggle('checked');
+    }
+};
+
+
+document.getElementById("btnRemove").addEventListener('click',function(){
+this.parentNode.remove();
+});
